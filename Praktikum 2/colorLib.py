abda56d2e547d4b61a614632d62eb7f84fa5b1ba
co1inco <colin.meihoefer@gmx.de>
2018-11-09 20:44:33 +0100

    updated python script

---
+++ Praktikum 2/colorLib.py
@@ -0,0 +1,31 @@
+
+def map(value, x1, x2, y1, y2):
+    return (value - x1) * (y2 - y1) / (x2 - x1) + y1
+    #(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+
+
+def hex2col(red, green, blue):
+        colors = 0x0000;
+        colors = colors | (int(map(red,   0, 255, 0, 31)) << 11)
+        colors = colors | (int(map(green, 0, 255, 0, 63)) << 6)
+        colors = colors |  int(map(blue,  0, 255, 0, 31))
+        return colors
+
+def splitColor(color):
+    return int(("%04x" % color)[0:2], 0x10), int(("%04x" % color)[2:4], 0x10)
+
+def printColor(c1, c2=-1, c3=-1):
+    if c2==-1:
+        print("%6x" % c1)
+    elif c3==-1:
+        print("%02x %02x" % (c1, c2))
+    else:
+        print("%02x %02x %02x" % ( c1,c2,c3))
+
+
+
+red2 = hex2col(255,0,0)
+blue2 = hex2col(0,0,255)
+green2 = hex2col(0,255,0)
+white2 = hex2col(255,255,255)
+black2 = hex2col(0,0,0)
--- Praktikum 2/cpp-p2.py
+++ Praktikum 2/cpp-p2.py
@@ -1,21 +1,9 @@
 import serial
+from colorLib import *
+import math
 
 port = 'COM1'
 baud = 9600
-
-
-def map(value, x1, x2, y1, y2):
-    return (value - x1) * (y2 - y1) / (x2 - x1) + y1
-    #(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-
-
-def hex2col(red, green, blue):
-        colors = 0b0000000000000000;
-        colors = int(colors, 2) | (int(map(red,   0, 255, 0, 31), 2) << 11);
-        colors = colors | (map(green, 0, 255, 0, 63) << 6);
-        colors = colors |  map(blue,  0, 255, 0, 31);
-        return colors
-    
 
 class serCom():
     def __init__(self, port='COM1', baud=9600, timeout=2):
@@ -36,33 +24,112 @@ class serCom():
 class Display():
     def __init__(self, port='COM1', baud=9600, timeout=2):
         self.ser = serCom(port, baud, timeout)
+        self.outType = "hex"
 
+    def clearScreen(self, color1=-1, color2=-1):
+        if color1 == -1:
+            color1 = 0x00
+            color2 = 0x00
+        elif color2==-1:
+            color1, color2 = splitColor(color1)
 
-    def drawRectangle(self, x1, y1, x2, y2, color1, color2, width=0):
-        if width > 0:
-            message = ("%02x%02x%02x%02x%02x%02x%02x%02x" % (0x03, x1, y1, x2, y2, color1, color2, width)).encode()
-        elif width == 0:
-            message = ("%02x%02x%02x%02x%02x%02x%02x" % (0x02, x1, y1, x2, y2, color1, color2)).encode()
+        if self.outType == "hex":
+            s = "%02x%02x%02x"
+        elif self.outType == "char":
+            s = "%c%c%c"
+
+        message = (s % (0x00, color1, color2)).encode()
         self.ser.write(message)
 
-    def drawText(self, x, y, color1, color2, color3, color4, text):
-        message = ("%02x%02x%02x%02x%02x%02x%02x%02x" % (0x05, x, y, len(text), color1, color2, color3, color4)) + text
+
+    def drawRectangle(self, x1, y1, x2, y2, color1, color2=-1, width=0):
+        if width > 0:
+            if self.outType == "hex":
+                s = "%02x%02x%02x%02x%02x%02x%02x%02x"
+            elif self.outType == "char":
+                s = "%c%c%c%c%c%c%c%c"
+        if width == 0:
+            if self.outType == "hex":
+                s = "%02x%02x%02x%02x%02x%02x%02x"
+            elif self.outType == "char":
+                s = "%c%c%c%c%c%c%c"
+            
+        if color2==-1:
+            color1, color2 = splitColor(color1)
+        if width > 0:
+            message = (s % (0x03, x1, y1, x2, y2, color1, color2, width)).encode()
+        elif width == 0:
+            message = (s % (0x02, x1, y1, x2, y2, color1, color2)).encode()
+        self.ser.write(message)
+        
+
+    def drawText(self, x, y, text, color1, color2, color3=-1, color4=-1):
+        if self.outType == "hex":
+            s = "%02x%02x%02x%02x%02x%02x%02x%02x"
+        elif self.outType == "char":
+            s = "%c%c%c%c%c%c%c%c"
+            
+        if color3==-1 or color4==-1:
+            color1, color2 = splitColor(color1)
+            color3, color4 = splitColor(color2)
+        message = (s % (0x05, x, y, len(text), color1, color2, color3, color4)) + text
         self.ser.write(message.encode())
 
+
     def getPoti(self):
-        self.ser.write("%02x" % 0x06)
+        if self.outType == "hex":
+            s = "%02x"
+        elif self.outType == "char":
+            s = "%c"
+            
+        self.ser.write((s % 0x06).encode())
         return int(self.ser.read().decode())
-        
+
+    def drawOval(self, x1, y1, x2, y2, color1, color2=-1, width=0, keepWidth=True): 
+        if color2==-1:
+            color1, color2 = splitColor(color1)
+
+        if self.outType == "hex":
+            s = "%02x%02x%02x%02x%02x%02x%02x"
+        elif self.outType == "char":
+            s = "%c%c%c%c%c%c%c"
+
+        if keepWidth:
+            x1 = x1+(y2-y1)
+            x2 = x2-(y2-y1)
+            
+        commands = []
+        if width == 0:
+            commands.append((s % (0x02, x1, y1, x2, y2, color1, color2)).encode())
+            for i in range(y1, y2, 2):
+                pos = round(math.sin(map(i, y1, y2, 0, math.pi))*width)
+                commands.append((s % (0x02, x1-pos-2, i, x1, i+1, color1, color2)).encode())
+                commands.append((s % (0x02, x2, i, x2+pos+2, i+1, color1, color2)).encode())
+
+        else:
+            commands.append((s % (0x02, x1, y1, x2, y1+width, color1, color2)).encode())
+            commands.append((s % (0x02, x1, y2-width, x2, y2, color1, color2)).encode())
+            for i in range(y1, y2, 2):
+                pos = round(math.sin(map(i, y1, y2, 0, math.pi))*width)
+                commands.append((s % (0x02, x1-pos-2, i, x1-pos, i+1, color1, color2)).encode())
+                commands.append((s % (0x02, x2+pos, i, x1+pos+2, i+1, color1, color2)).encode())
+
+        for i in commands:
+            self.ser.write(i)
+            
+            
 if __name__ == "__main__":
     
     dsp = Display()
 
-    dsp.drawRectangle(5,5,10,10,0,0xf8)
-    dsp.drawText(5,5, 0x00,0x00, 0xff, 0xff, "Hlihalo")
+    dsp.clearScreen()
+
+    dsp.drawRectangle(5,5,10,10,red2)
+    dsp.drawText(5,5, "Hlihalo", red2, white2)
     print(dsp.getPoti())
 
-    print(hex2col(255,0,255))
-
+    dsp.drawOval(5,5,10,10,red2)
+    
 
 
     
